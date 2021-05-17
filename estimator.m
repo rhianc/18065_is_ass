82c959dba9aab4e5bd6ae577c20e72882adb1cc0
Rhian Chavez <rhianchavez@Rhians-MacBook-Pro.local>
2021-05-17 18:29:55 -0400

    added estimator

--- estimator.m
+++ estimator.m
@@ -1,20 +1,29 @@
 
+% take as inputs x and y (input & output vectors) and returns the estimated transfer function. 
 
-iddata1 = iddata(y,x,1);        % y is output data, x is input data, 1 is 
-                                % sample period (discrete)
-                                
-                                % y and x are both column vectors of equal
-                                % length
+function best_sys = estimator(x,y)
 
-load iddata1 z1;                % iddata1 is time domain input/output data
-np = 5;                         % nubmer of poles
-sys = tfest(z1,np);             % the estimated transfer function
+    % get dataset length: 
+    length = size(y);
 
-%{
+    iddata1 = iddata(y,x,1);                                                % y is output data, x is input data, 1 is sample period (discrete)                    
 
-enter these in cmd line to see results
+    load iddata1 z1;                                                        % iddata1 is time domain input/output data
 
-sys.Numerator;                  
-sys.Denominator;
+    training_data = z1(1:floor(4*length/5));
+    validation_data = z1(floor(4*length/5)+1,length);
 
-%}
+    % try different order transfer functions and select minimal error system
+    best_fit = 0;
+    best_sys = tf();
+    for np = 1:10        
+        sys = tfest(training_data,np);                                      % estimate system with np poles 
+        [y,fit,ic] = compare(validation_data,sys);
+        if fit > best_fit
+            % choose best fit model by comparing to previous best fit
+            best_fit = fit;
+            best_sys = sys;
+        end
+    end
+end
+
