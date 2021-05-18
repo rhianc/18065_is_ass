d7f631087abfb654b308d8d899afe9b2aaea84c8
Rhian Chavez <rhianchavez@Rhians-MacBook-Pro.local>
2021-05-18 14:59:33 -0400

    kinda works

--- estimator.m
+++ estimator.m
@@ -2,29 +2,27 @@
 % take as inputs x and y (input & output vectors) and returns the estimated transfer function. 
 % estimator for single input single output system
 
-function best_sys = estimator(x,y)
+function best_sys = estimator(z1)
 
     % get dataset length: 
-    length = size(y);
-
-    iddata1 = iddata(y,x,1);                                                % y is output data, x is input data, 1 is sample period (discrete)                    
-
-    load iddata1 z1;                                                        % iddata1 is time domain input/output data
-
-    training_data = z1(1:floor(4*length/5));
-    validation_data = z1(floor(4*length/5)+1,length);
+    data_size = length(z1.InputData);
+    
+    training_data = z1(1:floor(4*data_size/5));
+    validation_data = z1(floor(4*data_size/5)+1:data_size);
 
     % try different order transfer functions and select minimal error system
-    best_fit = 0;
+    best_fit = -100;
     best_sys = tf();
-    for np = 1:10        
-        sys = tfest(training_data,np);                                      % estimate system with np poles 
-        [y,fit,ic] = compare(validation_data,sys);
+    for np = 1:15        
+        sys = tfest(z1,np);                                      % estimate system with np poles 
+        [y,fit,ic] = compare(z1,sys);
+        disp(fit);
         if fit > best_fit
             % choose best fit model by comparing to previous best fit
             best_fit = fit;
             best_sys = sys;
         end
     end
+    %plot(best_y);
 end
 
--- import_data.m
+++ import_data.m
@@ -1,6 +1,6 @@
 % function to import data for a given country
 
-function data = import_data(country)
+function data = import_data(country,in_or_out)
     
     temp = readtable('data.csv');
     
@@ -24,5 +24,10 @@ function data = import_data(country)
             end
         end
     end
-    data = temp.new_cases_smoothed(first_row:last_row);
+    if in_or_out == 1
+        data = temp.new_cases_smoothed(first_row:last_row);
+    else
+        data = temp.new_vaccinations_smoothed(first_row:last_row);
+    end
+    
 end
