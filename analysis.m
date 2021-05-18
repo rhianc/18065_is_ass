acc079f3760d79a818e354a5ecc0f5f51c9a67e6
Rhian Chavez <rhianchavez@DHCP-POOL-18-25-27-044.MIT.EDU>
2021-05-17 20:32:17 -0400

    added analysis

--- analysis.m
+++ analysis.m
@@ -16,7 +16,16 @@ country = "USA";
 sys_estimate = estimator(x,y);
 step_response = step(sys_estimate);
 
+% I actually think it won't work that great unless we use an initial
+% condition (like daily confirmed cases, and let the system see those
+% internal dynamics, then adjust for the new inputs once vaccinations start
+% happening)
+
+% I think the share of positive tests dataset is gonna end up being useful
+% for sure, but there are external factors that cause the peaks to start
+% decreasing without vaccinations which we somehow need to take into
+% account. I'm not sure which data to use as an input to account for this. 
+
 % we can make this all more complex (adding other inputs to the system,
 % non-linearity, etc) but first lets make sure it works as expected with
 % simple inputs. 
-
